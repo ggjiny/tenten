3c98a72ccbd6914e925277735fee0ea2886b1ed4
ggjiny <jnss599@gmail.com>
2024-04-29 17:55:41 +0900

    Fix: getCookies로 변경

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,10 +2,11 @@ import CardList from '@/src/components/card/CardList';
 import CustomCardList from '@/src/components/card/CustomCardList';
 import classNames from 'classnames';
 import getCookie from '@/src/lib/getCookie';
+import getCookies from '@/src/lib/getCookies';
 import styles from './page.module.scss';
 
 export default function Home() {
-  const userType = getCookie('userType');
+  const { userType } = getCookies();
 
   console.log('page', userType);
   return (
